b3415b4b5ffe29754d3f5a70ef4fece2f9a39570
matteo <matteodragonfire2312@gmail.com>
2024-10-15 15:15:09 +0200

    hask

---
+++ funzBase.hs
@@ -0,0 +1,19 @@
+myconcat :: [[a]] -> [a] 
+myconcat [] = []
+myconcat xs = foldl (++) [] xs 
+
+invert :: [a] -> [a]
+invert xs = foldr (\z ys -> ys ++ [z]) xs []
+
+myany :: (a -> Bool) -> [a] -> Bool
+myany f = foldr (||) False . map f 
+
+prova :: [Int] -> Bool
+prova xs = myall ( == 2) xs
+
+myall :: (a -> Bool) -> [a] -> Bool
+myall f = foldr (&&) True . map f 
+
+membro :: Eq a => a -> [a] -> Bool
+membro x ys = foldl (\x y -> x == y ) x ys
+
