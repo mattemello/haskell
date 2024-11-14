ef70268d844fa508879155c58b0bd0ba5ae20356
matteo <matteo.massimello@gmail.com>
2024-11-14 21:33:20 +0100

    io test with haskell

---
+++ io_frase.hs
@@ -0,0 +1,9 @@
+takeInput :: IO String
+takeInput =
+  getLine >>= \s ->
+    if null s
+      then return []
+      else takeInput >>= \st -> return (s ++ " " ++ st)
+
+main :: IO ()
+main = takeInput >>= (putStrLn)
